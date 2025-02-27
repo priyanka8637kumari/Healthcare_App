249e9053a299742b3fde04aa14c2d692394c78f4
minzikimi <minji.kim@hyperisland.se>
2025-02-27 10:44:01 +0100

    directory restructure

--- src/pages/Doctor/DoctorDashboard.jsx
+++ src/pages/Doctor/DoctorDashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SideNavDoc from "../components/SideNavDoctor/SideNavDoc"
 
 const DoctorDashboard = () => {
   return (
     <div>
-      
+      <SideNavDoc />
     </div>
   )
 }
