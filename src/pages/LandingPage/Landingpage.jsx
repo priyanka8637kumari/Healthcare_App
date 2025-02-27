d68398d161afda4fe90ba7e082807bc32ed14463
Priyanka <priyanka.kumari@hyperisland.se>
2025-02-27 11:47:15 +0100

    Components added to Landing page

--- src/pages/LandingPage/Landingpage.jsx
+++ src/pages/LandingPage/Landingpage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import LandingNavBar from "../../components/LandingPage/LandingNavbar/LandingNavbar";
+import LandingHero from "../../components/LandingPage/LandingHero/LandingHero";
+import LandingRole from "../../components/LandingPage/LandingRole/LandingRole";
 
 const Landingpage = () => {
   return (
     <div>
       <LandingNavBar />
+      <LandingHero />
+      <LandingRole />
     </div>
   )
 }
