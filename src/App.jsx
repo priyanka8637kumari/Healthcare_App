f701976cbb6e7415c9a77b7ae97d12c4ad7e800d
minzikimi <minji.kim@hyperisland.se>
2025-02-27 12:33:17 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        
-    
+
         <Route path="/doctor-dashboard" element={<DoctorDashboard />}> </Route>
       </Routes>
     </Router>
