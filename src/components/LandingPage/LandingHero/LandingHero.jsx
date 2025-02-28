934567840da4b05ea019a928bcc7f803b45c685a
Priyanka <priyanka.kumari@hyperisland.se>
2025-02-28 12:45:37 +0100

    background video updated

--- src/components/LandingPage/LandingHero/LandingHero.jsx
+++ src/components/LandingPage/LandingHero/LandingHero.jsx
@@ -6,10 +6,14 @@ import bgVideo from "../../../assets/landingPage/backgroundvideo.mp4";
 const HeroSection = () => {
     return (
       <section className={styles.hero} id="home">
-        <video  width="640" height="360" controls autoPlay loop>
+        {/* <video  width="640" height="360" controls autoPlay loop>
           <source src={bgVideo} type="video/mp4"/>
           Your browser does not support the video tag.
-        </video>
+        </video> */}
+        <video autoPlay loop muted className={styles.backgroundVideo}>
+        <source src={bgVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
         <div className={styles.heroContent}>
           <h1 className={styles.heroTitle}>Dummy text title</h1>
           <p className={styles.heroDescription}>
