db8df7bc51c3d44497761e3f1ccda11062ed6771
minzikimi <minji.kim@hyperisland.se>
2025-02-28 21:52:12 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import DoctorDashboard from './pages/Doctor/DoctorDashboard';
 import LoginDoc from './pages/Login/LoginDoc';
 import LabTechDashboard from './pages/LabTechnician/LabTechDashboard';
 import SignupDoc from './pages/Signup/SignupDoc';
+import MedicalRecordsDoc from './pages/Doctor/MedicalRecordsDoc ';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/login" element={<LoginDoc />} />
         <Route path="/signup" element={<SignupDoc />} />
         <Route path="/doctor-dashboard" element={<DoctorDashboard />}> </Route>
-
+        <Route path="/doctor-medicalrecords" element={<MedicalRecordsDoc />}> </Route>
         <Route path="/labtech-dashboard" element={<LabTechDashboard />}> </Route>
       </Routes>
     </Router>
