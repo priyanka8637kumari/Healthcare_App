4c04e5a41bab8b8276bf77cee66b1c6c81c991b9
Priyanka <priyanka.kumari@hyperisland.se>
2025-03-09 20:46:14 +0100

    bug fix

--- src/components/Doctor/HeaderDoctor/HeaderDoctor.jsx
+++ src/components/Doctor/HeaderDoctor/HeaderDoctor.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./HeaderDoctor.module.css"
 import doctorImg from "../../../assets/minji/doctorlayne.png"
-import arrowIcon from "../../../assets/minji/arrowIcon.png"
+import arrowIcon from "../../../assets/minji/arrowicon.png"
 import bellIcon from "../../../assets/minji/bellIcon.png"
 
 
